9bacfa7feb0764b1393f8b5aa01de27b5e2b7834
adriankorp <adrian.korpowski@gmail.com>
2024-06-10 14:25:30 +0200

    Add fetch

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
 
   config.secret_key = ''
   config.jwt do |jwt|
-    jwt.secret = Rails.application.config.jwt_secret
+    jwt.secret = ENV.fetch('JWT_SECRET')
     jwt.expiration_time = 1.month.to_i
   end
 
